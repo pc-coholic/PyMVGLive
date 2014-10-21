d14078d49a1c91acf21b972b6ddc8a0476f027d1
Martin Gross <martin@pc-coholic.de>
2014-10-20 22:17:40 -0400

    Prepare for PyPi-packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='PyMVGLive',
+    version='1.0.0',
+    description='get live-data from mvg-live.de',
+    author='pc-coholic',
+    author_email='martin@pc-coholic.de',
+    url='https://github.com/pc-coholic/PyMVGLive',
+    packages=['PyMVGLive'],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    license='?',
+    install_requires=['requests'],
+)
