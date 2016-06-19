e07c9603a9f43f4c03a72d583ab239c117c45721
Martin Gross <martin@pc-coholic.de>
2016-06-19 13:54:02 +0200

    Bumping to 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='PyMVGLive',
-    version='1.1.1',
+    version='1.1.2',
     description='get live-data from mvg-live.de',
     author='pc-coholic',
     author_email='martin@pc-coholic.de',
