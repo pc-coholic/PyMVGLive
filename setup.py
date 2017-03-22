c1a699cddc78a6b96ab9ec929c0fef8e371a4df3
Martin Gross <martin@pc-coholic.de>
2017-03-22 12:49:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='PyMVGLive',
-    version='1.1.2',
+    version='1.1.3',
     description='get live-data from mvg-live.de',
     author='pc-coholic',
     author_email='martin@pc-coholic.de',
